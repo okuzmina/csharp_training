4037733c400ab28bc493853b59cb81306815f5f4
Olga Kuzmina <olga.kuzmina@arcadia.spb.ru>
2020-09-13 17:49:48 +0300

    Task #5: Creation of TestBase class

--- addressbook-web-tests/addressbook-web-tests/ContactData.cs
+++ addressbook-web-tests/addressbook-web-tests/ContactData.cs
@@ -1,12 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace WebAddressbookTest
 {
-    class ContactData
+    public class ContactData
     {
         private string firstName;
         private string lastName;
